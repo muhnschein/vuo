515d9f0db34fc3be21edfe7f45f3d7aba9f30267
muhnschein <213744746+muhnschein@users.noreply.github.com>
2026-08-29 10:26:30 +0200

    Drop partially-vendored third_party sources (3/10)

--- third_party/qmetaobject/src/qtquickcontrols2.rs
+++
@@ -1,9 +0,0 @@
-/// Refer to the Qt documentation for QQuickStyle
-pub struct QQuickStyle {}
-
-impl QQuickStyle {
-    /// Refer to the Qt documentation for QQuickStyle::setStyle
-    pub fn set_style(style: &str) {
-        std::env::set_var("QT_QUICK_CONTROLS_STYLE", style);
-    }
-}
